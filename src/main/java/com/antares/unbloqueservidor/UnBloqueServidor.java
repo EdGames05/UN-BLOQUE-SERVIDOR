e25ab3e110d7a610e53521e0845577c40022e7c0
solomeo <edcompany5@gmail.com>
2022-09-03 01:43:25 -0600

    Configuración inicial del mod

---
+++ src/main/java/com/antares/unbloqueservidor/UnBloqueServidor.java
@@ -0,0 +1,8 @@
+package com.antares.unbloqueservidor;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(UnBloqueServidor.MODID)
+public class UnBloqueServidor {
+    public static final String MODID = "unbloqueservidor";
+}
